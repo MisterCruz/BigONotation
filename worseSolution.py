af1dde8d411fc09021e7e7370d1308e1867ef234
Rolando Cruz <pspnando@me.com>
2016-12-26 11:00:15 -0800

    add 'worst solution' code for greatest difference

--- worseSolution.py
+++ worseSolution.py
@@ -1,1 +1,8 @@
-
+def get_greatest_difference(nums):
+  greatest_difference = 0
+  for num1 in nums:
+    for num2 in nums:
+      difference = num1 - num2
+      if difference > greatest_difference:
+        greatest_difference = difference
+  return greatest_difference
