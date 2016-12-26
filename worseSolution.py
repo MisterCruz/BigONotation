e2be4d15b67b33277a584d19ed01bb2adfa95c4a
Rolando Cruz <pspnando@me.com>
2016-12-26 11:02:48 -0800

    Add big O notation for worst solution :pencil2:

--- worseSolution.py
+++ worseSolution.py
@@ -6,3 +6,9 @@ def get_greatest_difference(nums):
       if difference > greatest_difference:
         greatest_difference = difference
   return greatest_difference
+
+'''
+This code gets the job done by finding every possible difference and comparing it with each other and finding the max.
+
+While it gets the job done, it has a runtime of O(n²) because the outer loop runs n times and the inner loop runs for each time the outer loop is run (n * n).
+'''
